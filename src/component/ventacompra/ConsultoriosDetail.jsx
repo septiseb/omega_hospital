49c1a8f91bcda31b2177595beaa26f6785041533
septiseb <sebsepdus@hotmail.com>
2022-05-16 17:36:17 -0500

    water mark it was added and also justify politica de privacidad*

--- src/component/ventacompra/ConsultoriosDetail.jsx
+++ src/component/ventacompra/ConsultoriosDetail.jsx
@@ -8,6 +8,7 @@ const consultorio = [
         imageUrl:
             require("../../utils/img/consultorio/consultorio.jpeg"),
         plano: require('../../utils/img/consultorio/consultorio_205.pdf'),
+        available:true,
         wameUrl: "Me%20gustaria%20tener%20más%20información%20sobre%20el%20Consultorio%20205",
     },
     {
@@ -16,6 +17,7 @@ const consultorio = [
         imageUrl:
             require("../../utils/img/consultorio/consultorio.jpeg"),
         plano: require('../../utils/img/consultorio/consultorio_320.pdf'),
+        available:true,
         wameUrl: "Me%20gustaria%20tener%20más%20información%20sobre%20el%20Consultorio%20320",
     },
     {
@@ -24,6 +26,7 @@ const consultorio = [
         imageUrl:
             require("../../utils/img/consultorio/consultorio.jpeg"),
         plano: require('../../utils/img/consultorio/consultorio_321.pdf'),
+        available:true,
         wameUrl: "Me%20gustaria%20tener%20más%20información%20sobre%20el%20Consultorio%20321",
     },
     {
@@ -32,6 +35,7 @@ const consultorio = [
         imageUrl:
             require("../../utils/img/consultorio/consultorio.jpeg"),
         plano: require('../../utils/img/consultorio/consultorio_322.pdf'),
+        available:true,
         wameUrl: "Me%20gustaria%20tener%20más%20información%20sobre%20el%20Consultorio%20322",
     },
     {
@@ -40,6 +44,7 @@ const consultorio = [
         imageUrl:
             require("../../utils/img/consultorio/consultorio.jpeg"),
         plano: require('../../utils/img/consultorio/consultorio_323.pdf'),
+        available:true,
         wameUrl: "Me%20gustaria%20tener%20más%20información%20sobre%20el%20Consultorio%20323",
     },
     {
@@ -48,6 +53,7 @@ const consultorio = [
         imageUrl:
             require("../../utils/img/consultorio/consultorio.jpeg"),
         plano: require('../../utils/img/consultorio/consultorio_324.pdf'),
+        available:true,
         wameUrl: "Me%20gustaria%20tener%20más%20información%20sobre%20el%20Consultorio%20324",
     }
 ]
@@ -115,10 +121,13 @@ const ConsultorioDetail = () => {
                             className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8"
                         >
                             {consultorio.map((person) => (
-                                <li key={person.name} className="z-40">
+                                <li key={person.name} className="z-0">
                                     <div className="space-y-4">
-                                        <div className="aspect-w-3 aspect-h-2">
-                                            <img className="object-cover shadow-lg rounded-lg" src={person.imageUrl} alt="" />
+                                        <div className="aspect-w-3 aspect-h-2 ">
+                                            <div className="flex justify-center items-center">
+                                            <img className="object-cover shadow-lg rounded-lg backdrop-blur-xl drop-shadow-sm z-0"  src={person.imageUrl} alt={person.name} />
+                                            <img className="absolute z-40  opacity-50 w-1/3" src={require('../../utils/img/omega_logo.png')} alt="Omega Hospital No Disponible"/>
+                                            </div>
                                         </div>
 
                                         <div className="space-y-2">
@@ -128,7 +137,7 @@ const ConsultorioDetail = () => {
                                             </div>
                                             <ul className="flex space-x-5">
                                                 <li>
-                                                    <a href={person.plano} target="_blank" className="text-gray-400 hover:text-gray-500">
+                                                    <a href={person.plano} target="_blank" rel="noreferrer" className="text-gray-400 hover:text-gray-500">
                                                         <button
                                                             type="button"
                                                             className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-sky-600 hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
@@ -138,7 +147,7 @@ const ConsultorioDetail = () => {
                                                     </a>
                                                 </li>
                                                 <li>
-                                                    <a href={urlWame + person.wameUrl} target="_blank" className="text-gray-400 hover:text-gray-500">
+                                                    <a href={urlWame + person.wameUrl} target="_blank" rel="noreferrer" className="text-gray-400 hover:text-gray-500">
                                                         <button
                                                             type="button"
                                                             className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-sky-700 hover:bg-sky-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-600"
