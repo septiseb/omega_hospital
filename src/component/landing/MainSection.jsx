c8b7d749ceb5686afa1b684c80791cebc377346d
Sebastian Septien <sebsepdus@hotmail.com>
2022-06-27 16:18:35 -0500

    it is modify the carrousel

--- src/component/landing/MainSection.jsx
+++ src/component/landing/MainSection.jsx
@@ -2,29 +2,23 @@ import React from 'react';
 import Carrousel from '../Carousel';
 
 const MainSection = () => {
-    let imgArr = [require("../../utils/img/portada_logo.jpg")
+    let imgArr = [require("../../utils/img/mainSlider/hospital.jpeg")
         , require("../../utils/img/mainSlider/servicios_apertura.jpeg")]
 
-        
+
     return (
-        <div className="relative h-1/2">
-            <div className="absolute inset-x-0 bottom-0" />
-            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div className="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
-                    <div className="relative inset-0">
-                        <Carrousel interval={3000}>{imgArr.map((im,idx) => (<img
-                            className="h-full w-full object-cover"
-                            key={idx}
-                            src={im}
-                            alt="People working on laptops"
-                        />))}</Carrousel>
-
-
-                        <div className="absolute inset-0 bg-gradient-to-r from-sky-50 to-sky-50 mix-blend-multiply" />
-                    </div>
+        <>
+            <div className="flex justify-center item-center ">
+                <div className="flex item-center w-2/3">
+                    <Carrousel interval={3000}>{imgArr.map((im, idx) => (<img
+                        key={idx}
+                        src={im}
+                        alt="People working on laptops"
+                        className="rounded-lg"
+                    />))}</Carrousel>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
