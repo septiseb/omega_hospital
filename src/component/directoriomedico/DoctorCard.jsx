4ef581c17a27d553e7f86090b06adc1a7594c70e
Sebastian Septien <sebsepdus@hotmail.com>
2022-07-04 18:43:01 -0500

    final touches in the doctor card

--- src/component/directoriomedico/DoctorCard.jsx
+++ src/component/directoriomedico/DoctorCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { OfficeBuildingIcon, PhoneIcon } from '@heroicons/react/solid'
+import { MdLocalHospital } from "react-icons/md";
 
 
 
@@ -9,18 +10,18 @@ const DoctorCard = ({ doctors }) => {
             <ul className="mx-8 my-8 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                 {doctors.map((doctor, idx) => (
                     <li key={idx} className="col-span-1 bg-white rounded-lg border-sky-200 border shadow divide-y divide-gray-200">
-                        <div className="w-full flex items-center justify-between p-6 space-x-6">
-                            <img className="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0" src={doctor.imageUrl} alt="" />
+                        <div className="w-full flex items-center justify-between p-6 space-x-4">
+                            <MdLocalHospital className="w-8 h-8 flex-shrink-0 text-sky-600"  />
                             <div className="flex-1">
-                                <div className="flex items-center space-x-3">
-                                    <h3 className="text-gray-900 text-lg font-medium truncate">{doctor.Nombre} {doctor.Apellido}</h3>
+                                <div className="flex flex-col sm:flex-row items-center sm:space-x-3 truncate">
+                                    <h3 className="text-gray-900 text-lg font-medium ">{doctor.Nombre} {doctor.Apellido}</h3>
                                     <span className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
                                         {doctor.Cedula}
                                     </span>
                                 </div>
 
                                 <p className="mt-1 text-gray-800 text-m italic truncate">{doctor.Especialidad}{doctor.Subespecialidad ? " en " + doctor.Subespecialidad : null}</p>
-                                <p className="mt-2 text-gray-500 text-sm overflow-y-auto h-12 ">{doctor.Descripcion}</p>
+                                <p className="mt-2 text-gray-500 text-sm overflow-y-auto h-10 ">{doctor.Descripcion}</p>
 
                             </div>
 
