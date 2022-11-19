ddcb75dcd0dcf3963962a2e807c66e93fba1ab22
septiseb <sebsepdus@hotmail.com>
2022-11-18 18:58:30 -0600

    it was added the dotenv and change keys

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+require("dotenv").config();
 
 const container = document.getElementById('root')
 const root = createRoot(container);
